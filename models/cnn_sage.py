update: rename pooling; fixed: wrong dimension

models/cnn_sage.py
@@ -5,7 +5,7 @@ import torch.sparse as sp
 import math
 from .layers.graph_sage import SAGELayer
 from .layers import activation as Act
-from .layers.pooling import GlobalMaxPooling, GlobalAvgPooling, GlobalSumPooling
+from .layers.pooling import MaxPooling, AvgPooling, SumPooling
 
 class CNN_SAGE(nn.Module):
     def __init__(self, vocab_size, max_seq_len, drop_rate,
@@ -34,7 +34,7 @@ class CNN_SAGE(nn.Module):
         self.embedding = self.init_unit_embedding(init_weight=init_weight)
         self.cnn_layers = nn.ModuleList()
         self.sage_layers = nn.ModuleList()
-        self.max_pool = GlobalMaxPooling()
+        self.max_pool = MaxPooling()
 
         in_dim = self.embedding_dim
         for cnn_dim in self.cnn_dims:

models/layers/graph_sage.py
@@ -2,7 +2,7 @@ import torch
 import torch.nn as nn
 import torch.nn.functional as F
 import torch.sparse as sp
-from .pooling import GlobalMaxPooling, GlobalAvgPooling, GlobalSumPooling
+from .pooling import MaxPooling, AvgPooling, SumPooling
 import math
 
 
@@ -13,11 +13,11 @@ class SAGELayer(nn.Module):
         self.out_dim = out_dim
         self.activation = activation if activation is not None else torch.relu
         if pooling == 'max':
-            self.pooling = GlobalMaxPooling()
+            self.pooling = MaxPooling()
         elif pooling == 'mean':
-            self.pooling = GlobalAvgPooling()
+            self.pooling = AvgPooling()
         elif pooling == 'sum':
-            self.pooling = GlobalSumPooling()
+            self.pooling = SumPooling()
         else:
             raise ValueError()
 
@@ -35,7 +35,7 @@ class SAGELayer(nn.Module):
         inputs = inputs.unsqueeze(1)  # [b, 1, t, h1]
         A = A.unsqueeze(-1)  # [b, t, t, 1]
         outputs = A * inputs  # [b, t, t, h1]
-        outputs = self.pooling(outputs, -1)  # [b, t, h1]
+        outputs = self.pooling(outputs, 2)  # [b, t, h1]
         outputs = torch.cat([X, outputs], -1)  # [b, t, h0+h1]
         outputs = self.fc(outputs)  # [b, t, h]
         outputs = self.activation(outputs)  # [b, t, h]

models/layers/pooling.py
@@ -4,9 +4,9 @@ import torch.nn.functional as F
 import torch.sparse as sp
 import math
 
-class GlobalMaxPooling(nn.Module):
+class MaxPooling(nn.Module):
     def __init__(self):
-        super(GlobalMaxPooling, self).__init__()
+        super(MaxPooling, self).__init__()
 
     def forward(self, input, axis=1):
         """[summary]
@@ -17,14 +17,29 @@ class GlobalMaxPooling(nn.Module):
         Returns:
             [b, h] -- [description]
         """
-        assert len(input.shape) == 3
         output, _ = torch.max(input, axis)
         return output
 
 
-class GlobalAvgPooling(nn.Module):
+class AvgPooling(nn.Module):
     def __init__(self):
-        super(GlobalAvgPooling, self).__init__()
+        super(AvgPooling, self).__init__()
+
+    def forward(self, input, axis=1):
+        """[summary]
+        
+        Arguments:
+        
+        Returns:
+            [b, h] -- [description]
+        """
+        output = torch.mean(input, axis)
+        return output
+
+
+class SumPooling(nn.Module):
+    def __init__(self):
+        super(SumPooling, self).__init__()
 
     def forward(self, input, axis=1):
         """[summary]
@@ -35,24 +50,5 @@ class GlobalAvgPooling(nn.Module):
         Returns:
             [b, h] -- [description]
         """
-        assert len(input.shape) == 3
-        output = torch.mean(input, axis)
-        return output
-
-
-class GlobalSumPooling(nn.Module):
-    def __init__(self):
-        super(GlobalSumPooling, self).__init__()
-
-    def forward(self, input):
-        """[summary]
-        
-        Arguments:
-            input [b, t, h] -- [description]
-        
-        Returns:
-            [b, h] -- [description]
-        """
-        assert len(input.shape) == 3
-        output = torch.sum(input, 1)
+        output = torch.sum(input, axis)
         return output

models/rnn.py
@@ -4,7 +4,7 @@ import torch.nn.functional as F
 import torch.sparse as sp
 import math
 from .layers import activation as Act
-from .layers.pooling import GlobalMaxPooling, GlobalAvgPooling, GlobalSumPooling
+from .layers.pooling import MaxPooling, AvgPooling, SumPooling
 
 class RNN(nn.Module):
     def __init__(self, vocab_size, max_seq_len, drop_rate,
@@ -27,7 +27,7 @@ class RNN(nn.Module):
         self.embedding = self.init_unit_embedding(init_weight=init_weight)
         self.birnn = None  # to be replaced in subclass
 
-        self.max_pool = GlobalMaxPooling()
+        self.max_pool = MaxPooling()
         pred_act = getattr(Act, pred_act, nn.ELU)
 
         self.pred_dims = pred_dims

models/rnn_gat.py
@@ -5,7 +5,7 @@ import torch.sparse as sp
 import math
 from .layers import activation as Act
 from .layers.gat import GATLayer
-from .layers.pooling import GlobalMaxPooling, GlobalAvgPooling, GlobalSumPooling
+from .layers.pooling import MaxPooling, AvgPooling, SumPooling
 
 class RNN_GAT(nn.Module):
     def __init__(self, vocab_size, max_seq_len, drop_rate,
@@ -28,7 +28,7 @@ class RNN_GAT(nn.Module):
         self.birnn = None  # to be replaced in subclass
         self.gcn_layers = nn.ModuleList()
 
-        self.max_pool = GlobalMaxPooling()
+        self.max_pool = MaxPooling()
         self.dense = None
 
 

models/rnn_gcn.py
@@ -5,7 +5,7 @@ import torch.sparse as sp
 import math
 from .layers.gcn import GCNLayer
 from .layers import activation as Act
-from .layers.pooling import GlobalMaxPooling, GlobalAvgPooling, GlobalSumPooling
+from .layers.pooling import MaxPooling, AvgPooling, SumPooling
 
 class RNN_GCN(nn.Module):
     def __init__(self, vocab_size, max_seq_len, drop_rate,
@@ -32,7 +32,7 @@ class RNN_GCN(nn.Module):
         self.birnn = None  # to be replaced in subclass
         self.gcn_layers = nn.ModuleList()
 
-        self.max_pool = GlobalMaxPooling()
+        self.max_pool = MaxPooling()
 
         out_dim = 0
         for i in range(len(hidden_dims)-1):