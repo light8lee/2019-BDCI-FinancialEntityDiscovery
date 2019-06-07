aa5e56792ecd6199ae7856475ac25cf1902e05da
light8lee <light8lee@gmail.com>
2019-06-07 14:45:41 +0800

    update: more options to inputs of gnn

--- models/cnn_diffpool.py
+++ models/cnn_diffpool.py
@@ -12,7 +12,7 @@ class CNN_DiffPool(nn.Module):
                  embedding_dim, window_size, num_gnn_layer, gnn,
                  pred_dims:list, cnn_dims:list, ratio:float,
                  init_weight=None, activation=None, pred_act:str='ELU',
-                 freeze:bool=False, add_norm=False, **kwargs):
+                 freeze:bool=False, mode:str='add_norm', **kwargs):
 
         super(CNN_DiffPool, self).__init__()
         assert window_size % 2 == 1
@@ -29,7 +29,7 @@ class CNN_DiffPool(nn.Module):
         self.cnn_dims = cnn_dims
         self.freeze = freeze
         self.activation = activation
-        self.add_norm = add_norm
+        self.mode = mode
 
         self.embedding = self.init_unit_embedding(init_weight=init_weight)
         self.cnn_layers = nn.ModuleList()
@@ -37,16 +37,24 @@ class CNN_DiffPool(nn.Module):
         self.max_pool = MaxPooling()
 
         in_dim = self.embedding_dim
+        flat_in_dim = self.embedding_dim
         for cnn_dim in self.cnn_dims:
             self.cnn_layers.append(
                 nn.Conv1d(in_dim, cnn_dim,
                           kernel_size=window_size, stride=1, padding=window_size//2)
             )
             in_dim = cnn_dim
+            flat_in_dim += in_dim
 
-        if self.add_norm:
+        if self.mode == 'add_norm':
             self.norm = nn.LayerNorm(in_dim)
             self.res_weight = nn.Linear(self.embedding_dim, in_dim)
+        elif self.mode == 'concat':
+            in_dim = flat_in_dim
+        elif self.mode == 'origin':
+            pass
+        else:
+            raise ValueError()
 
         out_dim = 0
         in_size = max_seq_len
@@ -100,15 +108,24 @@ class CNN_DiffPool(nn.Module):
         """
         inputs = self.embedding(input_ids)  # [2b, t, e]
         outputs = inputs.transpose(-1, -2)  # [2b, e, t]
+        if self.mode == 'concat':
+            flat_outputs = [outputs]
+
         for conv1d in self.cnn_layers:
             outputs = F.dropout(outputs, p=self.drop_rate, training=self.training)
             outputs = conv1d(outputs)  # [2b, h, t]
             outputs = self.activation(outputs)
-        outputs = outputs.transpose(-1, -2)  # [2b, t, h1]
-        outputs = outputs * input_masks.unsqueeze(-1)  # [2b, t, h1]
-        outputs = outputs.contiguous().view(-1, 2*self.max_seq_len, self.cnn_dims[-1])  # [b, 2t, h1]
-        outputs = F.dropout(outputs, p=self.drop_rate, training=self.training)
-        if self.add_norm:
+            if self.mode == 'concat':
+                flat_outputs.append(outputs)
+
+        if self.mode == 'concat':
+            outputs = torch.cat(flat_outputs, 1)  # [2b, e+h, t]
+        outputs = outputs.transpose(-1, -2)  # [2b, t, h]
+        hidden_dim = outputs.shape[-1]
+        outputs = outputs * input_masks.unsqueeze(-1)  # [2b, t, h]
+        outputs = outputs.contiguous().view(-1, 2*self.max_seq_len, hidden_dim)  # [b, 2t, h]
+        if self.mode == 'add_norm':
+            outputs = F.dropout(outputs, p=self.drop_rate, training=self.training)
             outputs = self.norm(outputs+self.res_weight(inputs.view(-1, 2*self.max_seq_len, self.embedding_dim)))
 
         pooled_outputs = []
