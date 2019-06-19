fixed: wrong dimension; wrong type

dataset.py
@@ -49,8 +49,8 @@ def collect_multigraph(need_norm, concat_ab, batch):
         batch_inputs_b = t.from_numpy(np.array(batch_inputs_b)).long()
         batch_inputs = (batch_inputs_a, batch_inputs_b)
 
-        batch_mask_a = t.from_numpy(np.array(batch_mask_a)).long()
-        batch_mask_b = t.from_numpy(np.array(batch_mask_b)).long()
+        batch_mask_a = t.from_numpy(np.array(batch_mask_a)).float()
+        batch_mask_b = t.from_numpy(np.array(batch_mask_b)).float()
         batch_masks = (batch_mask_a, batch_mask_b)
 
     batch_adjs = []

models/abcnn_diffpool.py
@@ -143,7 +143,7 @@ class ABCNN1_DiffPool(nn.Module):
             outputs = torch.cat([outputs_a, outputs_b], 1)  # [b, 2t, h]
 
         hidden_dim = outputs.shape[-1]
-        inputs_masks = torch.cat([masks_a, masks_b], 1)  # [b, 2t]
+        input_masks = torch.cat([masks_a, masks_b], 1)  # [b, 2t]
         outputs = outputs * input_masks.unsqueeze(-1)  # [b, 2t, h]
         outputs = outputs.contiguous().view(-1, 2*self.max_seq_len, hidden_dim)  # [b, 2t, h]
 

models/layers/abcnn.py
@@ -14,8 +14,8 @@ class ABCNN1(nn.Module):
         self.activation = activation if activation is not None else torch.relu
 
         self.weight = nn.Parameter(torch.FloatTensor(max_seq_len, in_dim))
-        self.conv = nn.Conv2d(2, out_dim, kernel_size=(in_dim, window_size),
-                              padding=(0, window_size//2), stride=1, dilation=1)
+        self.conv = nn.Conv2d(2, out_dim, kernel_size=(window_size, in_dim),
+                              padding=(window_size//2, 0), stride=1, dilation=1)
         
         self.reset_parameters()
 
@@ -25,26 +25,24 @@ class ABCNN1(nn.Module):
             weight.data.uniform_(-std_div, std_div)
     
     def forward(self, xa, xb):
-        xa = xa.transpose(-1, -2)  # [b, e, t1]
-        xb = xb.transpose(-1, -2)  # [b, e, t2]
-        xa = xa.unsqueeze(-1)  # [b, e, t1, 1]
-        xb = xb.unsqueeze(-1)  # [b, e, t2, 1]
+        xa_expanded = xa.unsqueeze(-2)  # [b, t1, 1, e]
+        xb_expanded = xb.unsqueeze(1)  # [b, 1, t2, e]
 
-        attn = torch.pow(xa - xb.transpose(-1, -2), 2)  # [b, e, t1, t2]
-        attn = torch.sum(attn, 1)  # [b, t1, t2]
+        attn = torch.pow(xa_expanded - xb_expanded, 2)  # [b, t1, t2, e]
+        attn = torch.sum(attn, -1)  # [b, t1, t2]
         attn = 1 / (torch.sqrt(attn) + 1)  # [b, t1, t2]
 
         xa_attn = torch.matmul(attn, self.weight)  # [b, t1, e]
         xb_attn = torch.matmul(attn.transpose(-1, -2), self.weight)  # [b, t2, e]
 
-        xa_attn = xa_attn.transpose(-1, -2).unsqueeze(-1)  # [b, e, t1, 1]
-        xb_attn = xb_attn.transpose(-1, -2).unsqueeze(-1)  # [b, e, t2, 1]
 
-        xa = torch.cat([xa, xa_attn], axis=-1)  # [b, e, t, 2]
-        xb = torch.cat([xb, xb_attn], axis=-1)  # [b, e, t, 2]
+        xa = torch.stack([xa, xa_attn], 1)  # [b, 2, t, e]
+        xb = torch.stack([xb, xb_attn], 1)  # [b, 2, t, e]
 
-        conv_a = self.conv(xa).squeeze(1)  # [b, t, h]
-        conv_b = self.conv(xb).squeeze(1)  # [b, t, h]
+        conv_a = self.conv(xa)  # [b, h, t, 1]
+        conv_b = self.conv(xb)  # [b, h, t, 1]
+        conv_a = conv_a.squeeze(-1).transpose(-1, -2)  # [b, t, h]
+        conv_b = conv_b.squeeze(-1).transpose(-1, -2)  # [b, t, h]
 
         conv_a = self.activation(conv_a)
         conv_b = self.activation(conv_b)

train.py
@@ -1,4 +1,3 @@
-
 import models
 from proj_utils.files import save_ckpt, load_ckpt, load_config_from_json
 from proj_utils.configuration import Config
@@ -17,13 +16,14 @@ import torch.optim as optim
 from tensorboardX import SummaryWriter
 from sklearn.metrics import confusion_matrix
 from collections import Counter
+import pdb
 
 Precision = lambda tp, fp: tp / (tp + fp)
 Recall = lambda tp, fn: tp / (tp + fn)
 F1 = lambda p, r: ((2 * p * r) / (p + r)) if (p != 0) and (r != 0) else 0
 
 
-def infer(data, model, criterion, seq_len, cuda):
+def infer(data, model, criterion, cuda):
     features, targets = data
     batch_ids, batch_masks, batch_adjs = features
     labels = targets.numpy()
@@ -133,6 +133,7 @@ def train(args):
 
     epoch_loss = 10000
     best_f1 = 0
+    # pdb.set_trace()
     if args.log:
         writer = SummaryWriter(os.path.join(args.save_dir, 'logs'))
     for epoch in range(conti, conti+args.epoch):
@@ -152,7 +153,7 @@ def train(args):
                 optimizer.zero_grad()
 
                 with t.set_grad_enabled(phase == 'train'):
-                    result, loss = infer(data, model, criterion, model_config.seq_len, args.cuda)
+                    result, loss = infer(data, model, criterion, args.cuda)
                     if phase == 'train':
                         loss.backward()
                         optimizer.step()