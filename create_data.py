a7b2828d1af355bb92bf9c140ad019cd9abd8d6c
light8lee <light8lee@gmail.com>
2019-09-03 19:59:45 +0800

    update: remove emoji

--- create_data.py
+++ create_data.py
@@ -46,6 +46,14 @@ tag = re.compile(r'<(\d|[a-z".A-Z/]|\s)+>')
 ques = re.compile(r'[?#/]+')
 vx = re.compile(r'(v\d+)|(微信:\d+)')
 user = re.compile(r'@.*:')
+emoji = re.compile("["
+    "\U0001F600-\U0001F64F"  # emoticons
+    "\U0001F300-\U0001F5FF"  # symbols & pictographs
+    "\U0001F680-\U0001F6FF"  # transport & map symbols
+    "\U0001F1E0-\U0001F1FF"  # flags (iOS)
+    "\U00002702-\U000027B0"
+    "\U000024C2-\U0001F251"
+    "]+", flags=re.UNICODE)
 def clean(text):
     text = text.replace('&nbsp;', ' ')
     text = img.sub('', text)
@@ -55,6 +63,7 @@ def clean(text):
     text = ques.sub('', text)
     text = vx.sub('', text)
     text = user.sub('', text)
+    text = emoji.sub('', text)
     return text
 
 
